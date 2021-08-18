0fffb112ebfaa1377cfb045c8020a16a8b1db39b
fedotovdmitriy14 <fedotovdmitriy14@gmail.com>
2021-08-18 20:15:25 +0300

    test changes

--- polls/tests.py
+++ polls/tests.py
@@ -9,6 +9,8 @@ from rest_framework.test import APITestCase
 
 from . import models
 
+#
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
